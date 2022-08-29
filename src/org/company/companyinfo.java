9051bada4fedaa70ecc1ff4477c4bf5befb3fa25
Thirumuruganchandrasekaran <thirumuruganchandrasekaran09@gmail.com>
2022-08-29 17:18:20 +0530

    code added

---
+++ src/org/company/companyinfo.java
@@ -0,0 +1,24 @@
+package org.company;
+
+public class companyinfo {
+
+
+public void companyName(){
+	System.out.println("companyname");
+}
+public void companyId(){
+	System.out.println("companyId");
+}
+public void companyAddress(){
+	System.out.println("companyaddress");
+}
+
+public static void main(String[] args) {
+companyinfo com = new companyinfo();
+	
+	com.companyName();
+	com.companyId();
+	com. companyAddress();
+	
+}
+}
